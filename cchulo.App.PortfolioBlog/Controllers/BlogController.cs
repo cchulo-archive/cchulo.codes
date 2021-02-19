8c694b6f073d7b4d4680146a4da6fe5ae5b88cb8
Carlos Chulo <cchulo.dev@gmail.com>
2021-02-19 07:50:28 -0800

    added GraphQL client, but need to translate the schema to .NET, at some point i should write a program that transpiles JSON to .NET and maybe Typescript, but will research to see if something like this already exists

--- cchulo.App.PortfolioBlog/Controllers/BlogController.cs
+++ cchulo.App.PortfolioBlog/Controllers/BlogController.cs
@@ -1,8 +1,13 @@
 ﻿using cchulo.App.PortfolioBlog.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using GraphQL;
 
 namespace cchulo.App.PortfolioBlog.Controllers
 {
@@ -13,17 +18,46 @@ namespace cchulo.App.PortfolioBlog.Controllers
 
         IConfiguration _serverConfigRef;
         IHttpClientFactory _httpClientFactoryRef;
+        ILogger<BlogController> _logger;
 
         private int port = 0;
 
-        public BlogController(IConfiguration serverConfig, IHttpClientFactory clientFactory)
+        public BlogController(IConfiguration serverConfig, IHttpClientFactory clientFactory, ILogger<BlogController> logger)
         {
             _serverConfigRef = serverConfig;
             _httpClientFactoryRef = clientFactory;
+            _logger = logger;
 
             port = _serverConfigRef.GetValue<int>("StrapiPort");
         }
 
+        [HttpGet]
+        public async Task<IActionResult> QueryBlogs()
+        {
+            try
+            {
+                // HttpClient client = _httpClientFactoryRef.CreateClient();
+                GraphQLHttpClient client = new GraphQLHttpClient($"http://localhost:{port}/graphql", new NewtonsoftJsonSerializer());
+
+                GraphQLRequest query = new GraphQLRequest(@"
+                    query {
+                        articles {
+                            id
+                        }
+                    }
+                ");
+
+                // var response = await client.SendQueryAsync<ResponseOwnerCollectionType>(query);
+
+                return Ok();
+
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+
+                return BadRequest();
+            }
+        }
 
     }
 }
