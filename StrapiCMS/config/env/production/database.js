2ce61383de5940861df4c613af7d156aa99afc86
Carlos Chulo <cchulo.dev@gmail.com>
2021-05-01 14:19:53 -0700

    making it so the information is being read from environment variables and not hard coded

--- StrapiCMS/config/env/production/database.js
+++ StrapiCMS/config/env/production/database.js
@@ -10,8 +10,8 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'pgstrapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         ssl: false,
